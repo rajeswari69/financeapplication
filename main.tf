5fbf907f0c47d9888c97d4c8fbf84daf800b3694
root <root@ip-172-31-39-85.ap-south-1.compute.internal>
2023-05-06 15:43:57 +0000

    add pem

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ provider "aws" {
 resource "aws_instance" "ec2-server" {
    ami = "ami-02eb7a4783e7e9317"
    instance_type = "t2.micro"
-   key_name = "mykey"
+   key_name = "banking"
    vpc_security_group_ids = ["sg-0078d46d0981fea1c"]
    provisioner "remote-exec" {
       inline = ["echo 'waituntil ssh is ready'"]
       connection {
            type = "ssh"
            user = "ubuntu"
-           private_key = file("mykey.pem")
+           private_key = file("banking.pem")
            host = aws_instance.ec2-server.public_ip
       }
    }
