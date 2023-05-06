2fbd664c662d9caac0df9822a4f04e540015cc49
root <root@ip-172-31-39-85.ap-south-1.compute.internal>
2023-05-06 06:47:54 +0000

    add updates

--- main.tf
+++ main.tf
@@ -1,24 +1,10 @@
 terraform {
-  backend "remote" {
-    organization = "hashicorp-v2"
-
-    workspaces {
-      name = "terraform-provider-aws-repository"
-    }
-  }
-
   required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "5.24.0"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
   }
-
-  required_version = ">= 0.13.5"
-}
-
-provider "github" {
-  owner = "hashicorp"
 }
 provider "aws" {
     profile = "default"
